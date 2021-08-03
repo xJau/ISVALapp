f2dcf447396957cf04facf05244fe1dd647494c0
n.brancatisano@mindgear.it <n.brancatisano@mindgear.it>
2021-08-03 15:56:49 +0100

    added colorscheme

--- lib/Routes/dashboard.dart
+++ lib/Routes/dashboard.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:isval_test/Components/header.dart';
 import 'package:isval_test/Components/record_list.dart';
+import 'package:isval_test/Utility/colorscheme.dart';
 
 class DashboardRoute extends StatelessWidget {
   const DashboardRoute();
@@ -8,6 +9,8 @@ class DashboardRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: Header(), body: Center(child: RecordList(RecordType.STOCK)));
+        backgroundColor: Colorscheme.SFONDO_PAGINA,
+        appBar: Header(),
+        body: Center(child: RecordList(RecordType.STOCK)));
   }
 }
